ed71ede03be32d08b59a5f5b7d6bb71ad551f2f1
Tiago Cardoso Cavaleiro <99280896+tiagocardosoc@users.noreply.github.com>
2022-08-19 09:24:11 -0300

    atualizando nome do header

--- src/middlewares/autenticarToken.js
+++ src/middlewares/autenticarToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken")
 
 export default function autenticarToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
+    const authHeader = req.headers['Authorization'];
     const token = authHeader && authHeader.split(" ")[1];
 
     if (!token) {
