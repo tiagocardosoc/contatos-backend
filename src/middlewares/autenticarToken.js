f3cdcbb6a3354e887e33350d8d8cc0485525c616
Tiago Cardoso Cavaleiro <99280896+tiagocardosoc@users.noreply.github.com>
2022-08-26 09:09:55 -0300

    Retirando consoles

--- src/middlewares/autenticarToken.js
+++ src/middlewares/autenticarToken.js
@@ -4,11 +4,6 @@ export default function autenticarToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(" ")[0];
 
-    'localStorage:'
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZmU5ODU4NmIwNTc0N2Q3ZGZjZjFjMiIsImlhdCI6MTY2MTMwMTg0M30.a60CIyxlUP6e6hHV-9dJsq6SvffTWvQw7vSpumAIjBE' 
-    'enviado pro back:' 
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZmU5ODU4NmIwNTc0N2Q3ZGZjZjFjMiIsImlhdCI6MTY2MTMwMDY5M30.r7tkpTvO4lZpZqiFuFQM1ZMV3A7gtWC5gAl2V4hQLP8'
-
     if (!token) {
         return res.status(401).json({
             mensagem: 'Acesso negado.'
